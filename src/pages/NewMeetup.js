b7b19fdac019780507e6eb884c7b28177617e650
Turi <40395348+turinaf@users.noreply.github.com>
2022-05-25 12:00:25 +0800

    useNavigate after sending POST request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,7 @@
+import { useNavigate } from 'react-router-dom'
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetupPage(){
-
+    const navigate =  useNavigate();
      function addMeetupHandler(meetupData){
           fetch('https://react-tut-58c6d-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json', {
                method: 'POST',
@@ -9,7 +10,9 @@ function NewMeetupPage(){
                     'Content-Type': 'application/json'
                }
           }
-          );
+          ).then(() => {
+               navigate('/');
+          });
      }
     return (
     <section>
